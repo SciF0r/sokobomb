4c7df518b3a3e4c197e63a32480a5e9f6770dfea
chbruderer <ch.bruderer@gmail.com>
2013-03-27 18:26:17 +0100

    Added menu Buttons with text

---
+++ src/ch/bfh/sokobomb/model/MenuButton.java
@@ -0,0 +1,63 @@
+package ch.bfh.sokobomb.model;
+
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.AngelCodeFont;
+import org.newdawn.slick.SlickException;
+
+public class MenuButton {
+	private String text;
+	
+	private int x,y, width, height;
+	
+	public String getText() {
+		return text;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public MenuButton(int x, int y, int width, int height, String text){
+		this.text =text;
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+	}
+	
+	public void draw(){
+		GL11.glColor3f(0.5f, 0.9f, 0.5f);
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2i(x,y);
+		GL11.glVertex2i(x,y+height);
+		GL11.glVertex2i(x+width,y+height);
+		GL11.glVertex2i(x+width,y);
+		GL11.glEnd();
+		
+		AngelCodeFont fnt=null;
+		try {
+			fnt = new AngelCodeFont("res/font/sokofont.fnt", "res/font/sokofont_0.png");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		fnt.drawString(x+20, y+20, text);
+		
+	}
+	
+	
+	
+
+}
--- src/ch/bfh/sokobomb/states/PauseState.java
+++ src/ch/bfh/sokobomb/states/PauseState.java
@@ -3,20 +3,24 @@ package ch.bfh.sokobomb.states;
 import java.io.IOException;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
 import ch.bfh.sokobomb.model.Bomb;
+import ch.bfh.sokobomb.model.Coordinate;
 import ch.bfh.sokobomb.model.Field;
 import ch.bfh.sokobomb.model.FieldItem;
-import ch.bfh.sokobomb.util.Tiles;
+import ch.bfh.sokobomb.model.MenuButton;
 
 public class PauseState extends State {
 
-	private int xmin, xmax, ymin, ymax;
+	private MenuButton mb;
 	
 	public PauseState(Field field) {
 		super(field);
+		mb = new MenuButton(50, 70, 300, 80, "Return to the Game");
+		
 	}
 	
 	@Override
@@ -29,10 +33,42 @@ public class PauseState extends State {
 	}
 	
 	@Override
-	public void handleLeftClick(int x, int y) {
-		System.out.println(x +" "+y);
-		if (xmin < x && xmax > x && ymin < y && ymax > y ){
+	public void handleLeftClick(Coordinate coordinate) {
+		int x=coordinate.getX();
+		int y=coordinate.getY();
+		if (mb.getX() < x && mb.getX()+mb.getWidth() > x && mb.getY() < y && mb.getY() + mb.getHeight() > y ){
 			super.field.setState(new PlayState(field));
+		}
+	}
+	
+	@Override
+	public void pollInput() {
+		Keyboard.enableRepeatEvents(true);
+
+		// Left mouse button
+		if (Mouse.isButtonDown(0)) {
+			int x = Mouse.getX();
+			int y = (this.field.getHeight() - Mouse.getY());
+			System.out.println(x +" "+ y);
+			this.handleLeftClick(new Coordinate(x, y));
+		}
+
+		// Right mouse button
+		if (Mouse.isButtonDown(1)) {
+			int x = Mouse.getX();
+			int y = (this.field.getHeight() - Mouse.getY());
+			this.handleRightClick(new Coordinate(x, y));
+		}
+
+		while (Keyboard.next()) {
+			// Key was pressed
+			if (Keyboard.getEventKeyState()) {
+				this.handleKeyPress(Keyboard.getEventKey());
+			}
+			// Key was released
+			else {
+				this.handleKeyRelease(Keyboard.getEventKey());
+			}
 		}
 	}
 	
@@ -48,31 +84,20 @@ public class PauseState extends State {
 
 		super.field.getPlayer().draw();
 		
+		
 		//Draw Background
-		GL11.glColor4f(1.0f, 0.0f, 0.0f, 0.6f);		
+		GL11.glColor4f(0.5f, 0.70f, 0.5f, 0.6f);		
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glVertex2i(0,0);
 		GL11.glVertex2i(0,field.getHeight());
 		GL11.glVertex2i(field.getWidth(), field.getHeight());
 		GL11.glVertex2i(field.getWidth(),0);
 		GL11.glEnd();
-		
-		//Draw Button
-		xmin=50;
-		ymin=70;
-		xmax=150;
-		ymax=170;
-		GL11.glColor3f(1.0f, 0.0f, 1.0f);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2i(xmin,ymin);
-		GL11.glVertex2i(xmin,ymax);
-		GL11.glVertex2i(xmax,ymax);
-		GL11.glVertex2i(xmax,ymin);
-		GL11.glEnd();
-		
+		 
+		mb.draw();
 		Display.update();
 
-		super.pollInput();
+		this.pollInput();
 	}
 	
 	
