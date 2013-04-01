39d7850d7f39bfc7c2257178aa1c6f14d97ca412
Denis Simonet <denis.simonet@adfinis-sygroup.ch>
2013-04-01 15:43:55 +0200

    Initial import of Drawable

---
+++ src/ch/bfh/sokobomb/model/Drawable.java
@@ -0,0 +1,13 @@
+package ch.bfh.sokobomb.model;
+
+import java.io.IOException;
+
+/**
+ * Interface for classes which can be drawn
+ *
+ * @author Denis Simonet
+ */
+interface Drawable {
+
+	public void draw() throws IOException;
+}
