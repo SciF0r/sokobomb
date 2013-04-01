49cc39b89b0b07a822ee06da1b3b9a97f0fbfac0
Denis Simonet <denis.simonet@adfinis-sygroup.ch>
2013-04-01 16:00:32 +0200

    Fixed PauseState

--- src/ch/bfh/sokobomb/states/PauseState.java
+++ src/ch/bfh/sokobomb/states/PauseState.java
@@ -8,6 +8,7 @@ import org.lwjgl.opengl.GL11;
 import ch.bfh.sokobomb.model.Coordinate;
 import ch.bfh.sokobomb.model.Field;
 import ch.bfh.sokobomb.model.MenuButton;
+import ch.bfh.sokobomb.util.Tiles;
 
 /**
  * Shows the pause screen
@@ -18,27 +19,28 @@ import ch.bfh.sokobomb.model.MenuButton;
 public class PauseState extends State {
 
 	private MenuButton mb;
-	
+
 	public PauseState(Field field) {
 		super(field);
 		mb = new MenuButton(50, 70, 300, 80, "Return to the Game");
 	}
-	
+
 	@Override
 	public void handleKeyPress(int key) {
 		switch (key) {
-			case Keyboard.KEY_RIGHT:
-				super.field.movePlayer(1, 0);
+			case Keyboard.KEY_ESCAPE:
+				super.field.setState(new PlayState(super.field));
 				break;
 		}
 	}
-	
+
 	@Override
 	public void handleLeftClick(Coordinate coordinate) {
-		int x = coordinate.getX();
-		int y = coordinate.getY();
-		if (mb.getX() < x && mb.getX() + mb.getWidth() > x && mb.getY() < y && mb.getY() + mb.getHeight() > y ) {
-			super.field.setState(new PlayState(field));
+		int x = coordinate.getX() * Tiles.HEIGHT;
+		int y = coordinate.getY() * Tiles.WIDTH;
+
+		if (mb.getX() < x && (mb.getX() + mb.getWidth()) > x && mb.getY() < y && (mb.getY() + mb.getHeight()) > y) {
+			super.field.setState(new PlayState(super.field));
 		}
 	}
 
