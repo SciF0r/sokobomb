50cc00cac85b12e1e506e6869f7db2ebceb1b446
Denis Simonet <denis.simonet@adfinis-sygroup.ch>
2013-04-06 16:10:30 +0200

    Initial import of FieldController

---
+++ src/ch/bfh/sokobomb/controller/FieldController.java
@@ -0,0 +1,69 @@
+package ch.bfh.sokobomb.controller;
+
+import java.io.IOException;
+
+import ch.bfh.sokobomb.field.Field;
+import ch.bfh.sokobomb.field.PlayField;
+import ch.bfh.sokobomb.parser.Token;
+
+/**
+ * Manage fields
+ *
+ * @author Denis Simonet
+ */
+public class FieldController {
+
+	private Field field;
+
+	public FieldController(Field initialField) {
+		this.field = initialField;
+	}
+
+	/**
+	 * @param field Sets the field to be drawn
+	 */
+	public void setField(Field field) {
+		this.field = field;
+	}
+
+	/**
+	 * @return The field
+	 */
+	public Field getField() {
+		return this.field;
+	}
+
+	/**
+	 * Says the field to be drawn
+	 */
+	public void draw() {
+		this.field.draw();
+	}
+
+	/**
+	 * Draws the field
+	 */
+	public void drawField() {
+		try {
+			this.field.drawField();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	/**
+	 * @param token The token to be added to the field
+	 */
+	public void addItemByToken(Token token) {
+		this.field.addItemByToken(token);
+	}
+
+	/**
+	 * Starts a new game
+	 */
+	public void startGame() {
+		PlayField playField = (PlayField)this.field;
+		playField.startGame();
+	}
+}
