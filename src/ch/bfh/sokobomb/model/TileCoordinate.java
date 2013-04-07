89d5adf2c28311eada9be895120f0e11a8804ce9
Denis Simonet <denis.simonet@adfinis-sygroup.ch>
2013-04-07 13:48:46 +0200

    Added TileCoordinate

---
+++ src/ch/bfh/sokobomb/model/TileCoordinate.java
@@ -0,0 +1,16 @@
+package ch.bfh.sokobomb.model;
+
+/**
+ * Stores a tile coordinate
+ *
+ * @author Denis Simonet
+ */
+public class TileCoordinate extends Coordinate {
+
+	public TileCoordinate(int x, int y) {
+		super(
+			x,
+			y
+		);
+	}
+}
